c971407f7cc262fc1b615f2c3537d5baae7f47e4
Wejden <ferjeniwejden0@gmail.com>
2025-12-13 00:03:51 +0100

    fix router to let Symfony handle AssetMapper assets

--- router.php
+++ router.php
@@ -1,16 +1,16 @@
 <?php
 
 // Router script for PHP built-in server
-// Handles static files and Symfony routing
+// All requests go through Symfony (including AssetMapper assets)
 
 $requestUri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-// Serve static files directly
-if ($requestUri !== '/' && file_exists(__DIR__ . '/public' . $requestUri)) {
+// Serve static files from public/ directory only (not /assets, those go through Symfony)
+if ($requestUri !== '/' && !str_starts_with($requestUri, '/assets/') && file_exists(__DIR__ . '/public' . $requestUri)) {
     return false;
 }
 
-// All other requests go to Symfony front controller
+// All other requests (including /assets/*) go to Symfony front controller
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/public/index.php';
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 
